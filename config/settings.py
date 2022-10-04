f01fac9df18f0ac9d568aeb791eb129417b3ea1b
rjsdn9403 <hyk95070@naver.com>
2022-10-04 10:09:45 +0900

    project

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ from pickle import TRUE
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -25,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q4zq!zb-g=6gle50pm*g^rme$i5rtoj5()1as2y5k71*^3559h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eosam1017.pythonanywhere.com']
 
 
 # Application definition
@@ -126,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
